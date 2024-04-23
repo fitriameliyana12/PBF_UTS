7bfa1665259fe12848d68bbc8ca027b283353816
anisashafira <anisashafira07@gmail.com>
2024-04-23 23:18:32 +0700

    ultrasonic

--- uts/src/app/ultrasonic/page.tsx
+++ uts/src/app/ultrasonic/page.tsx
@@ -16,7 +16,7 @@ const cardData: CardProps[] = [
         icon: DollarSign
     },
     {
-        discription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        discription: "Sensor ultrasonik adalah perangkat elektronik yang digunakan untuk mendeteksi jarak atau keberadaan benda dengan menggunakan gelombang ultrasonik. Prinsip kerja sensor ultrasonik didasarkan pada pengiriman pulsa gelombang ultrasonik ke objek yang ingin dideteksi, dan kemudian mengukur waktu yang dibutuhkan untuk gelombang ultrasonik tersebut kembali setelah dipantulkan oleh objek.a",
         amount: " ",
         label: "Deskripsi",
         icon: Users
