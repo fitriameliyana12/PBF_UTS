7cc7fbdb018d39b5824cfae1c916a021da2ae1ca
Yusfi Arif <arifyusfi1@gmail.com>
2024-04-24 07:28:38 +0700

    dashboard

--- uts/src/app/page.tsx
+++ uts/src/app/page.tsx
@@ -2,7 +2,6 @@ import PageTitle from "@/components/PageTitle";
 import Image from "next/image";
 import { DollarSign, Users, CreditCard, Activity } from "lucide-react";
 import Card, { CardContent, CardProps } from "@/components/Card";
-import BarChart from "@/components/BarChart";
 import SalesCard, { SalesProps } from "@/components/SalesCard";
 import { CarouselDemo } from "@/components/carousel";
 
